0e67a279f26bd119169de60b8f348fa0ba3ba79c
Francisca Cruzat <franciscacruzat23@gmail.com>
2019-06-29 16:22:49 -0400

    Devise user agregado y todo lo que eso conlleva

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :reviews
   resources :services
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "services#index"
+
 end
