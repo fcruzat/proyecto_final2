70c323b49a37619da2243332ed6c1ba46e00b990
Francisca Cruzat <franciscacruzat23@gmail.com>
2019-07-08 19:30:05 -0400

    Active admin instalado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'admin_prueba@mail.com', password: 'contraseña.admin', password_confirmation: 'contraseña.admin') if Rails.env.development?
