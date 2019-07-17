63f2316291d812970a3b426fdea9197ed05b3dbb
Francisca Cruzat <franciscacruzat23@gmail.com>
2019-07-17 19:06:41 -0400

    Eliminada la migración con cambio de datos en born(costó)

--- db/migrate/20190717193027_change_born_to_be_date_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeBornToBeDateInUsers < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :born, :date
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_193027) do
+ActiveRecord::Schema.define(version: 2019_07_16_162802) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -104,7 +104,6 @@ ActiveRecord::Schema.define(version: 2019_07_17_193027) do
     t.string "photo"
     t.string "country"
     t.string "city"
-    t.date "born"
     t.text "description"
     t.boolean "approbated", default: false
     t.datetime "created_at", null: false
@@ -119,6 +118,7 @@ ActiveRecord::Schema.define(version: 2019_07_17_193027) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.integer "born"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
