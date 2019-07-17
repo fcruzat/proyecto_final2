1ff95fd5befb7bd730d3aef857a46f79ec160e79
Francisca Cruzat <franciscacruzat23@gmail.com>
2019-07-17 19:35:37 -0400

    Admin sin el if de development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin_prueba@mail.com', password: 'contraseña.admin', password_confirmation: 'contraseña.admin') if Rails.env.development?
+AdminUser.create!(email: 'admin_prueba@mail.com', password: 'contraseña.admin', password_confirmation: 'contraseña.admin')
