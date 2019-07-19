1a1af3b57d86a4f9c1ff8425ea9e1b51d94d5455
Francisca Cruzat <franciscacruzat23@gmail.com>
2019-07-18 22:59:10 -0400

    Cambio en el dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -9,24 +9,38 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    # Here is an example of a simple dashboard with columns and panels.
+    
+
+    columns do
+      column do
+        panel "Últimos servicios agregados" do
+          ul do
+            Service.last(5).map do |service|
+              li link_to(service.name, admin_service_path(service))
+            end
+          end
+        end
+      end
+      column do
+        panel "Usuarios" do
+          ul do
+            li "Usuarios registrados: #{User.count}"
+            li "Administradores registrados: #{AdminUser.count}"
+          end
+        end
+      end
+      column do
+        panel "Servicios" do
+          ul do
+            li "Servicios creados: #{Service.count}"
+          end
+        end
+      end
+    end
+  end
 end
+
+   
